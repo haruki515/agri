1affb59b773256a8b31558e96ee5d85e5b48cbc7
Hiroto Naya <hiroto.naya.78@gmail.com>
2017-04-21 01:19:09 +0900

    build部分作成

--- worker/bin/yoshida.rb
+++ worker/bin/yoshida.rb
@@ -12,7 +12,8 @@ T = YAML.load_file('../tables/scheme.yml')
 DB_CONF = YAML.load_file('../conf.yml')["database"]
 SEED_LIST = YAML.load_file('../conf.yml')["seed"]
 BUILD_LIST = YAML.load_file('../conf.yml')["build"]
-CSV_PATH = "/data/csv/"
+CSV_PATH = "/data/worker/data/"
+SQL_PATH = "/data/worker/tables/build/"
 
 # TODO ログを吐けるようにする
 class BuildDb
@@ -25,25 +26,44 @@ class BuildDb
   end
 
   def seed_table(table)
-    puts "---------------" + "START " + table["table_name"] + "---------------"
+    puts "START " + table["table_name"]
     @connection.exec("DROP TABLE IF EXISTS " + table["table_name"])
 
     # テーブル作成
-    puts "Create table " + table["table_name"]
     query = createTableStatement(table)
     @connection.exec(query)
-    puts "...Done."
 
     # import data
-    puts "Import data to " + table["file_name"]
+    puts importData(CSV_PATH, table["file_name"], table["header"])
     @connection.exec(importData(CSV_PATH, table["file_name"], table["header"]))
-    puts "...Done."
-    puts "---------------" + "END " + table["table_name"] + "---------------"
+    puts "END " + table["table_name"]
+  end
+
+  def build_table(sql_path, sql)
+    puts "START " + sql
+    @connection.exec(getSql(sql_path + sql))
+    puts "END " + sql
   end
 end
 
-## main TODO ループにする
 database = BuildDb.new
+# テーブル作成 & テーブルに生データimport
+puts "=============================================="
+puts "                  START SEED                  "
+puts "=============================================="
 for target in SEED_LIST do
   database.seed_table(T["schema"][target])
 end
+puts "=============================================="
+puts "                  END SEED                  "
+puts "=============================================="
+puts "=============================================="
+puts "                  START BUILD                 "
+puts "=============================================="
+## 生テーブルから検索テーブル作成
+for sql in BUILD_LIST do
+  database.build_table(SQL_PATH, sql)
+end
+puts "=============================================="
+puts "                  END BUILD                 "
+puts "=============================================="
